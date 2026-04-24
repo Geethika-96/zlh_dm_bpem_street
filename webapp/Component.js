fec4b254441ca3b823f904601918edbfaf47b179
Geethika-96 <geethikat@londonhydro.com>
2026-04-24 14:43:01 +0530

    First commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.lh.sapui5.zlhdmbpemstreet.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
